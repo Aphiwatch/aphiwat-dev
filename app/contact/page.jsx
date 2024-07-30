a37447f9799d7f602d65b2b8ce5cec2b9ca0f152
Aphiwatch <aphiwatch09@gmail.com>
2024-07-31 00:08:07 +0700

    a little change

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -46,7 +46,7 @@ const Contact = () => {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c]
             rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Lets work together</h3>
               <p className="text-white/60">
               Fill out the form and I'll get back to you soon as possible. 
               </p>
